e1042a9c210dc2b932d7181d826cfdafa345b7c2
Torri Raines <raines.torri@gmail.com>
2019-01-22 16:11:02 -0500

    map working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const mapboxgl = require("mapbox-gl");
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";
+mapboxgl.accessToken = "pk.eyJ1IjoicmFpbnlsZWFmIiwiYSI6ImNqcjg4OW5tNTA0OHY0MHNiYmtoN3IxbXcifQ.L_ckflCEpn5QpY0MsxZI_w";
 
 const map = new mapboxgl.Map({
   container: "map",
@@ -8,3 +8,10 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.641, 41.895] for Chicago
